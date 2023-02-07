30ee527e68649dcf1d746653e01382d62bacc327
DisuAde <idrisadekunle726@gmail.com>
2023-02-07 19:23:14 +0100

    init,router

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import navbarVue from './components/navbar.vue';
+</script>
+
+<template>
+  <div>
+    <navbar-vue/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,33 @@
+<template>
+   
+   <div class="container" :style="{ backgroundImage: `url(${bg})`}" >
+<img :src="logo1" alt="image">
+
+<div class="line" > </div>
+
+   </div>
+
+</template>
+
+<script>
+import logo1 from '../assets/starter-code/assets/shared/logo.svg'
+import bg from '../assets/starter-code/assets/home/background-home-desktop.jpg'
+export default {
+
+data(){
+   return{
+      logo1,bg
+   }
+}
+}
+</script>
+<style scoped>
+
+.line{
+   width: 50%;
+}
+.container{
+   min-height: 100vh;
+   width: 100%;
+}
+</style>
